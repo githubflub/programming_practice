399a8c3417dfdc0b7462aa32308e245998c869a7
Cuad <18061353+githubflub@users.noreply.github.com>
2019-12-15 10:42:28 -0800

    Update rotateMatrix.js

--- javascript/00_arrays_and_strings/rotateMatrix.js
+++ javascript/00_arrays_and_strings/rotateMatrix.js
@@ -11,14 +11,29 @@ for (let i = 0; i < MATRIX_SIZE; i++) {
 }
 
 // print matrix
-printMatrix(my_matrix)
+printMatrix(my_matrix, 'Here is your matrix')
+
+// rotate matrix
+printMatrix(rotateMatrix(my_matrix), "Here is your rotated matrix")
 
 function rotateMatrix(matrix) {
+   const n = MATRIX_SIZE - 1;
 
+   for (let i = 0; i < MATRIX_SIZE; i++) {
+      for (let j = i; j < n - i; j++) {
+         let swap = matrix[i][j];
+         matrix[i][j] = matrix[n - j][i];
+         matrix[n - j][i] = matrix[n - i][n - j]
+         matrix[n - i][n - j] = matrix[j][n - i]
+         matrix[j][n - i] = swap;
+      }
+   }
+
+   return matrix;
 }
 
-function printMatrix(matrix) {
-   console.log(`Here is your matrix`);
+function printMatrix(matrix, message = '') {
+   console.log(message);
 
    for (let i = 0; i < MATRIX_SIZE; i++) {
       let row = '  '
